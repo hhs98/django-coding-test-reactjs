e87dac9ad171c9be2074e4ed80de59eb2e18c85e
Siju <siju35-168@diu.edu.bd>
2023-07-11 15:23:16 +0600

    feat: added product list view with pagination

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,11 @@
 from django.views import generic
-
-from product.models import Variant
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from product.models import (
+    Variant,
+    Product,
+    ProductVariant,
+    ProductVariantPrice,
+)
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +16,26 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+
+class ProductListView(generic.TemplateView):
+    template_name = 'products/list.html'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = Product.objects.all()
+        print(len(products))
+        page = self.request.GET.get('page')
+        paginator = Paginator(products, self.paginate_by)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+        context['product'] = True
+        context['products'] = products
+        context['paginator'] = paginator
         return context
